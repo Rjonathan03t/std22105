1e1961c155ec3ab2315970c6adab2192a66ee12a
Jonathan Rajao <hei.tovomihaja@gmail.com>
2025-02-05 08:06:32 +0300

    refact: change place

--- src/main/java/com/secu/demo/endpoint/rest/controller/health/Hello.java
+++ src/main/java/com/secu/demo/endpoint/rest/controller/health/Hello.java
@@ -1,9 +1,7 @@
-package com.secu.demo.endpoint;
+package com.secu.demo.endpoint.rest.controller.health;
 
 import com.secu.demo.PojaGenerated;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
