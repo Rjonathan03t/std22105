66001cfe473340b88206d3147e420d98d79f712d
Jonathan Rajao <hei.tovomihaja@gmail.com>
2025-02-05 08:12:09 +0300

    add some code

--- src/main/java/com/secu/demo/endpoint/rest/controller/health/Hello.java
+++ src/main/java/com/secu/demo/endpoint/rest/controller/health/Hello.java
@@ -2,6 +2,8 @@ package com.secu.demo.endpoint.rest.controller.health;
 
 import com.secu.demo.PojaGenerated;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 public class Hello {
+    public static final ResponseEntity<String> OK = new ResponseEntity<>("OK", HttpStatus.OK);
+    public static final ResponseEntity<String> KO =
+            new ResponseEntity<>("KO", HttpStatus.INTERNAL_SERVER_ERROR);
+
     @GetMapping("/hello")
     public String HelloWorld() {
         return "world";
